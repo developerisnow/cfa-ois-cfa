0008a382bd1b03a12ebde782c721b78572475eb6
developerisnow <35399970+developerisnow@users.noreply.github.com>
2025-11-11 19:49:13 +0300

    feat(deploy): [co-3a68] - Enable monorepo Docker builds for web apps • Compose apps build from repo root; Dockerfiles copy apps+packages feat(keycloak): [co-3a68] - Bootstrap tunnel/public redirect URIs for clients feat(cors): [co-3a68] - Add AllowedOrigins for public and tunnel in gateway agentID=co-3a68

--- ops/keycloak/bootstrap-realm.sh
+++ ops/keycloak/bootstrap-realm.sh
@@ -9,6 +9,10 @@ REALM=${REALM:-ois}
 ISSUER_URL=${ISSUER_URL:-http://87.249.49.56:53001}
 INVESTOR_URL=${INVESTOR_URL:-http://87.249.49.56:53002}
 BACKOFFICE_URL=${BACKOFFICE_URL:-http://87.249.49.56:53003}
+# Опционально добавить локальные туннельные адреса для редиректов
+ISSUER_TUNNEL_URL=${ISSUER_TUNNEL_URL:-http://localhost:155101}
+INVESTOR_TUNNEL_URL=${INVESTOR_TUNNEL_URL:-http://localhost:155102}
+BACKOFFICE_TUNNEL_URL=${BACKOFFICE_TUNNEL_URL:-http://localhost:155103}
 CLIENT_SECRET=${CLIENT_SECRET:-secret}
 
 KCADM=/opt/keycloak/bin/kcadm.sh
@@ -23,13 +27,14 @@ fi
 create_client() {
   local cid=$1
   local url=$2
-  local id=$(${KCADM} create clients -r ${REALM} -s clientId=${cid} -s enabled=true -s protocol=openid-connect -s publicClient=false -s 'redirectUris=["'${url}'/*"]' -s 'webOrigins=["*"]' -i)
+  local tunnel_url=$3
+  local id=$(${KCADM} create clients -r ${REALM} -s clientId=${cid} -s enabled=true -s protocol=openid-connect -s publicClient=false -s 'redirectUris=["'${url}'/*","'${tunnel_url}'/*"]' -s 'webOrigins=["*"]' -i)
   ${KCADM} update clients/${id}/client-secret -r ${REALM} -s value=${CLIENT_SECRET}
 }
 
-create_client portal-issuer ${ISSUER_URL}
-create_client portal-investor ${INVESTOR_URL}
-create_client backoffice ${BACKOFFICE_URL}
+create_client portal-issuer ${ISSUER_URL} ${ISSUER_TUNNEL_URL}
+create_client portal-investor ${INVESTOR_URL} ${INVESTOR_TUNNEL_URL}
+create_client backoffice ${BACKOFFICE_URL} ${BACKOFFICE_TUNNEL_URL}
 
 # Create demo users
 if ! ${KCADM} get users -r ${REALM} -q username=investor >/dev/null 2>&1; then
@@ -46,4 +51,3 @@ if ! ${KCADM} get users -r ${REALM} -q username=backoffice >/dev/null 2>&1; then
 fi
 
 echo "Realm '${REALM}' bootstrapped with clients and demo users."
-
